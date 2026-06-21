26d577c9af0238deade0ca074f0a3ec6aa05b7c6
Juan Carlos Alvarado <juanca.race.jc@gmail.com>
2026-06-20 23:46:18 -0400

    chore(security): ajustar SecurityConfig y filtros JWT para habilitar pruebas de integracion en Postman

--- src/main/java/com/gymapp/ms_tienda/config/SecurityConfig.java
+++ src/main/java/com/gymapp/ms_tienda/config/SecurityConfig.java
@@ -15,9 +15,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
